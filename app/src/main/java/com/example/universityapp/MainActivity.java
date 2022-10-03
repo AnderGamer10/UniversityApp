2e52fbc4d26eace6e42e4afc77c64677a6f24bf1
2dam3 <2dam3@HZ114485.H012108.NET>
2022-10-03 10:02:14 +0200

    first commit

---
+++ app/src/main/java/com/example/universityapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.universityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnMostrar;
+    EditText idPais, idNombre;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnMostrar = findViewById(R.id.btnMostrar);
+        idNombre = findViewById(R.id.idNombre);
+        idPais = findViewById(R.id.idPais);
+
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LeerApi();
+            }
+        });
+    }
+    private void LeerApi() {
+
+        String url = "http://universities.hipolabs.com/search?country=spain";
+
+        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    //text.setText(jsonObject.getString("userId"));
+//                    String title = jsonObject.getString("title");
+//                    text.setText(title);
+                    //text.setText(jsonObject.getString("body"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(postResquest);
+    }
+
+}

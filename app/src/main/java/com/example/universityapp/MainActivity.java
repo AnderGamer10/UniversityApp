8e2265bf77d67615616a6b8b9df8011ed19487b4
2dam3 <2dam3@HZ114485.H012108.NET>
2022-10-04 12:32:53 +0200

    universidades

--- app/src/main/java/com/example/universityapp/MainActivity.java
+++ app/src/main/java/com/example/universityapp/MainActivity.java
@@ -17,15 +17,25 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.IDN;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    interface Universidad{
+        String nombre = "";
+        String domain = "";
+    }
     Button btnMostrar;
     EditText idPais, idNombre;
+    TextView prueba;
+    String strPais, strNombre;
+    ArrayList<String> datosUniversidades = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,24 +43,36 @@ public class MainActivity extends AppCompatActivity {
         btnMostrar = findViewById(R.id.btnMostrar);
         idNombre = findViewById(R.id.idNombre);
         idPais = findViewById(R.id.idPais);
+        prueba = findViewById(R.id.prueba);
 
         btnMostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                strPais = String.valueOf(idPais.getText());
+                strNombre = String.valueOf(idNombre.getText());
                 LeerApi();
             }
         });
     }
     private void LeerApi() {
 
-        String url = "http://universities.hipolabs.com/search?country=spain";
+        String url = "http://universities.hipolabs.com/search?country="+ strPais + "&name=" + strNombre;
 
         StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    //text.setText(jsonObject.getString("userId"));
+                    JSONArray jsonObject = new JSONArray(response);
+                    for (int i = 0; i < jsonObject.length();i++){
+                        JSONObject j = jsonObject.getJSONObject(i);
+                        Log.i("cositas", j.getString("name"));
+//                        datosUniversidades.add(new Universidad(j.getString("name"),j.getString("domains")));
+                        datosUniversidades.add(j.getString("name"));
+                    }
+                    for (String nombreUni: datosUniversidades){
+                        Log.i("cositas2", nombreUni);
+                    }
+//                    prueba.setText(jsonObject.getString("name"));
 //                    String title = jsonObject.getString("title");
 //                    text.setText(title);
                     //text.setText(jsonObject.getString("body"));
